254380ec1b8d2fbcdfe38847d7a73a248f1f6e0f
Dave Hill <dave@davehill.org>
2020-12-22 14:06:17 +0000

    output.tf

---
+++ output.tf
@@ -0,0 +1,34 @@
+# VPC
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+# CIDR blocks
+output "cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.cidr_block
+}
+
+# Subnets
+output "private_cidr" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_cidr
+}
+
+output "public_cidr" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_cidr
+}
+
+# NAT gateways
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
+}
+
+# AZs
+output "az" {
+  description = "A list of availability zones spefified as argument to this module"
+  value       = module.vpc.az
+}
